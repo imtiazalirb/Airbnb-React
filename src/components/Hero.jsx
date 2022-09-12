17fed1687484bc983564980fbd242975252334b7
Imtiaz Ali <imtiazali4090@gmail.com>
2022-09-12 22:33:36 +0600

    feat: hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Heroimage from "/src/assets/hero.png"
+
+function Hero() {
+    return(
+        <div className='hero--section'>
+             <img src={ Heroimage } alt="" className='hero--image' />
+             <h1 className='hero--header'>Online Experiences</h1>
+             <p className='hero--text'>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+        </div>
+    )
+}
+
+export default Hero
