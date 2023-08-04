f64c74c808684077ef317afc1ccb45f97262f34a
Imtiaz <55402109+imtiazalirb@users.noreply.github.com>
2023-08-05 04:26:18 +0600

    refactor: badgetext is called from badgeText  variable

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 
 
 function Card(props){
+    let badgeText
+    if (props.openSpots == 0)
+    {
+        badgeText = "SOLD OUT"
+    }
+    else if (props.location == "Online")
+    {
+        badgeText = "ONLINE"
+    }
     return(
         <div className='card'>
-            {props.openSpots == 0 && <div className='card--badge'>SOLD OUT</div>}
+            {badgeText && <div className='card--badge'>{badgeText}</div>}
             <img src={`../public/images/${props.img}`} className='card--image'/>
             <div className='card--stats'>
                 <img src='../public/images/star.png' className='star'/>
