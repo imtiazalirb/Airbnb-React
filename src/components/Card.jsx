866d78d5370035d761209100a27a1420423ffd2d
Imtiaz <55402109+imtiazalirb@users.noreply.github.com>
2023-08-04 16:55:40 +0600

    style: sold out badge

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 function Card(props){
     return(
         <div className='card'>
+            {props.openSpots == 0 && <div className='card--badge'>SOLD OUT</div>}
             <img src={`../public/images/${props.img}`} className='card--image'/>
             <div className='card--stats'>
                 <img src='../public/images/star.png' className='star'/>
